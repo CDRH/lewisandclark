7019f63099c92581593de98674271411b6e433a6
Jessica Dussault <jdussault4@gmail.com>
2016-11-04 14:10:53 -0500

    changes routes and filenames

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,31 @@
 Rails.application.routes.draw do
   root 'static#index', as: :home
-  get 'about' => 'static#about', as: :about
 
-  #kmd's temp pages, will move around as necessary
+  get 'about' => 'static#about', as: :about
+  get 'journals' => 'static#journals', as: :journals
+  get 'journals/about' => 'static#journals_about', as: :j_about
+  get 'journals/calendar' => 'static#calendar', as: :calendar
+  get 'journals/contents' => 'static#contents', as: :contents
   get 'map' => 'static#map', as: :map
   get 'multimedia' => 'static#multimedia', as: :multimedia
-  get 'images' => 'static#images', as: :images
-  get 'images_maps' => 'static#images_maps', as: :images_maps
-  get 'images_people_places' => 'static#images_people_places', as: :images_people_places
-  get 'images_plants_animals' => 'static#images_plants_animals', as: :images_plants_animals
   get 'texts' => 'static#texts', as: :texts
-  get 'journals' => 'static#journals', as: :journals
-  get 'journals_date' => 'static#journals_date', as: :j_date
-  get 'journals_toc' => 'static#journals_toc', as: :j_toc
-  get 'journals_about' => 'static#journals_about', as: :j_about
 
-  # items
-  get 'browse' => 'items#browse', as: :browse
-  get 'search' => 'items#search', as: :search
-  get 'item/:id' => 'items#show', as: :item,  :constraints => { :id => /[^\/]+/ }
+  # images
+  get 'images' => 'static#images', as: :images
+  get 'images/maps' => 'static#images_maps', as: :images_maps
+  get 'images/people_places' => 'static#images_people_places', as: :images_people_places
+  get 'images/plants_animals' => 'static#images_plants_animals', as: :images_plants_animals
 
   # indices (facets for journal terms)
   get 'journals/index' => 'indices#index', as: :index
   get 'journals/index/native_nations' => 'indices#nations', as: :index_nations
   get 'journals/index/people' => 'indices#people', as: :index_people
   get 'journals/index/places' => 'indices#places', as: :index_places
+
+  # items
+  get 'browse' => 'items#browse', as: :browse
+  get 'search' => 'items#search', as: :search
+  get 'item/:id' => 'items#show', as: :item,  :constraints => { :id => /[^\/]+/ }
 
   # errors
   match '/404', to: 'errors#not_found', via: :all
