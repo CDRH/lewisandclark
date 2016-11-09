41eaf3a00afb2c369f249e6fc8623084c71779fc
Greg Tunink <techgique@gmail.com>
2016-11-09 13:20:43 -0600

    Fix typo in parameter check

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -63,7 +63,7 @@ class ItemsController < ApplicationController
     end
     # if lc_searchtype_s "journal file" specifically selected (unlikely),
     # then allow through, otherwise always omit from search
-    if options["lc_searchtype_s"] != "journal file"
+    if options["lc_searchtype_s"] != "journal_file"
       fq << 'lc_searchtype_s:(NOT "journal_file")'
     end
     options[:fq] = fq
