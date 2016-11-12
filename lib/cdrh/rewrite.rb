fbe90a664dfcae649567d4362c423f4a2f34ea95
karindalziel <kdalziel2@unl.edu>
2016-11-11 20:48:40 -0600

    from rosie

--- lib/cdrh/rewrite.rb
+++ lib/cdrh/rewrite.rb
@@ -4,7 +4,10 @@
 # Matching YAML file format for Rack::Rewrite with feature subset
     # Method may only be "rewrite" or "r30[1,2,3,7,8]" syntax
 # URL rewrites and redirects with regexes work, but no procs or sending files
-# Addition - May specify to drop the query string with options['no_qs']
+
+# Additions
+# - May specify to drop the query string with options['no_qs']
+# - Rewrites are checked again before returned to prevent multiple redirects
 
 require 'yaml'
 
@@ -12,6 +15,7 @@ module CDRH
     class Rewrite
         # Variables
         @@rewrites = nil
+        @@uri_prefix = nil
 
         # Save rewrites as class variable to only load once at server start
         if @@rewrites.nil?
@@ -34,6 +38,20 @@ module CDRH
 
         protected
         def _call(env)
+            # Determine if URI prefix upon receiving first request
+            if @@uri_prefix.nil?
+                request = Rack::Request.new(env)
+
+                if request.path_info != request.path
+                    # Remove last occurrence of path_info
+                    # so removing / from /prefix/ is /prefix
+                    @@uri_prefix = request.path.sub(/(.*)(#{request.path_info})(.*)/, '\1\3')
+                    puts "CDRH::Rewrite - URI prefix set to '#{@@uri_prefix}'"
+                else
+                    @@uri_prefix = ''
+                end
+            end
+
             response = !@@rewrites.nil? \
                 ? route_request(env) \
                 : nil
@@ -45,12 +63,19 @@ module CDRH
             end
         end
 
-        def compute_to(from, path_qs, to)
-            if from.is_a?(Regexp) && from.match(path_qs)
+        def compute_to(from, request, path_qs, to)
+            # Prepend URI prefix if present
+            to = @@uri_prefix + to if !@@uri_prefix.blank?
+
+            if from.is_a?(Regexp) && (from.match(request.path_info) || from.match(path_qs))
+                matched_path = from.match(request.path_info) \
+                    ? from.match(request.path_info) \
+                    : from.match(path_qs)
+
                 computed_to = to.dup
-                computed_to.gsub!("$&", from.match(path_qs).to_s)
-                (from.match(path_qs).size - 1).downto(1) do |num|
-                    computed_to.gsub!("$#{num}", from.match(path_qs)[num].to_s)
+                computed_to.gsub!("$&", matched_path.to_s)
+                (matched_path.size - 1).downto(1) do |num|
+                    computed_to.gsub!("$#{num}", matched_path[num].to_s)
                 end
 
                 return computed_to
@@ -59,18 +84,18 @@ module CDRH
             end
         end
 
-        def match_options_not?(from_not, r, path_qs)
+        def match_options_not?(from_not, request, path_qs)
             from_not.is_a?(Regexp) \
-            && (r.path.match(from_not) || path_qs.match(from_not)) \
-            || (r.path === from_not || path_qs === from_not) \
+            && (request.path_info.match(from_not) || path_qs.match(from_not)) \
+            || (request.path_info === from_not || path_qs === from_not) \
                 ? true \
                 : false
         end
 
         def route_request(env)
-            r = Rack::Request.new(env)
-            path_qs = r.path || ''
-            qs = r.query_string || ''
+            request = Rack::Request.new(env)
+            path_qs = request.path_info || ''
+            qs = request.query_string || ''
             if not qs.empty?
                 path_qs += '?'+ qs
             end
@@ -80,52 +105,62 @@ module CDRH
                     ? rewrite['options'] \
                     : {}
 
-                next if options['host'] && !r.host.match(options['host'])
+                next if options['host'] && !request.host.match(options['host'])
                 next if options['method'] && !env['REQUEST_METHOD'].match(options['method'])
-                next if options['scheme'] && !r.scheme.match(options['scheme'])
+                next if options['scheme'] && !request.scheme.match(options['scheme'])
 
                 from = rewrite['from']
                 to = rewrite['to']
                 from_not = options['not'] || ''
 
-                if rule_matches?(from, r, path_qs, from_not)
+                if rule_matches?(from, request, path_qs, from_not)
                     # Response variables
                     body = []
                     headers = {}
 
                     # Rewrite variables
                     method = rewrite['method']
-                    to = compute_to(rewrite['from'], path_qs, to)
-                    if !qs.empty? && !options['no_qs']
-                        to += '?'+ qs
+                    to = compute_to(rewrite['from'], request, path_qs, to)
+                    to_qs = (!qs.empty? && !options['no_qs']) \
+                        ? to +'?'+ qs \
+                        : to
+
+                    if to === request.path
+                        puts "CDRH::Rewrite - Skipping rule that redirects to self in infinte loop"
+                        puts "  Request Path: #{request.path}"
+                        puts "  Rule From: #{from}"
+                        puts "  Rule To: #{rewrite['to']}"
+                        puts "    Evals: #{to}"
+                        next
                     end
 
                     if method =~ /r30[12378]/
                         status = method[1..-1]
 
-                        headers['Location'] = to
+                        headers['Location'] = to_qs
                         headers['Content-Type'] = Rack::Mime.mime_type(File.extname(to), fallback='text/html')
 
-                        body.push %Q{Redirecting to <a href="#{to}">#{to}</a>}
+                        body.push %Q{Redirecting to <a href="#{to_qs}">#{to_qs}</a>}
 
-                        puts "CDRH::Rewrite - Redirecting #{path_qs} to #{to}"
+                        puts "CDRH::Rewrite - Redirecting #{path_qs} to #{to_qs}"
 
                         return [status, headers, body]
                     elsif method == "rewrite"
-                        env['REQUEST_URI'] = to
-                        if q_index = to.index('?')
-                            env['PATH_INFO'] = to[0..q_index-1]
+                        env['REQUEST_URI'] = to_qs
+                        if q_index = to_qs.index('?')
+                            env['PATH_INFO'] = to_qs[0..q_index-1]
                             env['QUERY_STRING'] = (!options['no_qs']) \
-                                ? to[q_index+1..-1] \
+                                ? to_qs[q_index+1..-1] \
                                 : ''
                         else
-                            env['PATH_INFO'] = to
+                            env['PATH_INFO'] = to_qs
                             env['QUERY_STRING'] = ''
                         end
 
-                        puts "CDRH::Rewrite - Rewriting #{path_qs} to #{to}"
+                        puts "CDRH::Rewrite - Rewriting #{path_qs} to #{to_qs}"
 
-                        return nil
+                        # Recheck rules after rewrite
+                        return route_request(env)
                     end
                 end
             end
@@ -133,13 +168,13 @@ module CDRH
             return nil
         end # /route_request
 
-        def rule_matches?(from, r, path_qs, from_not)
+        def rule_matches?(from, request, path_qs, from_not)
             from.is_a?(Regexp) \
-            &&  ((r.path.match(from) || path_qs.match(from)) \
-                && !match_options_not?(from_not, r, path_qs) \
+            &&  ((request.path_info.match(from) || path_qs.match(from)) \
+                && !match_options_not?(from_not, request, path_qs) \
             ) \
-            || ((r.path === from || path_qs === from) \
-                && !match_options_not?(from_not, r, path_qs) \
+            || ((request.path_info === from || path_qs === from) \
+                && !match_options_not?(from_not, request, path_qs) \
             ) \
                 ? true \
                 : false
