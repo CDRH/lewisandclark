ed1efb057dfc516f086e39fccc017bb61fd45698
Jessica Dussault <jdussault4@gmail.com>
2016-11-16 12:08:03 -0600

    fixes facet arrow collapse directions

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,11 +4,13 @@ $(function() {
     $this = $(this)
     if ( $this.find('.glyphicon-chevron-right').length > 0 ) {
       $this.find('.glyphicon-chevron-right')
-        .toggleClass("glyphicon-chevron-down")
+        .toggleClass("glyphicon-chevron-down", true)
+        .toggleClass("glyphicon-chevron-right", false);
     }
     else {
       $this.find('.glyphicon-chevron-down')
-      .toggleClass("glyphicon-chevron-right")
+      .toggleClass("glyphicon-chevron-right", true)
+      .toggleClass("glyphicon-chevron-down", false);
     }
   });
 });
